00dcad102f4b77594d60aebf0b80b9f489192dd1
DmitriyShvedov <89867550+DmitriyShvedov@users.noreply.github.com>
2021-12-09 11:20:20 +0200

    bad_pass

--- WebTestsEducation/src/test/java/shvedov/qa/vk/TestSeleniumClass.java
+++ WebTestsEducation/src/test/java/shvedov/qa/vk/TestSeleniumClass.java
@@ -8,7 +8,7 @@ public class TestSeleniumClass extends TestBaseConfig {
 
     public static void main(String[] args) throws InterruptedException {
 
-        AuthorizationVK p1 = new AuthorizationVK("shvedov19957@gmail.com", "x2g-cds-5cY-HmA");
+        AuthorizationVK p1 = new AuthorizationVK("shvedov19957@gmail.com", "bad_pass");
         AuthorizationVK p2 = new AuthorizationVK("shvedov772", "tfbwl3ehwankdfsil");
         MessageVkTo m1 = new MessageVkTo("Алиме Дудак", "Привет) это прога. ЯТЛ");
         MessageVkTo m2 = new MessageVkTo("Нигматзянов Дмитрий", "Привет, я программа, если хочешь, гоу общаться!");
